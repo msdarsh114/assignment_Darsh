1cee071389df544c42a232b66e92f824fa8ee8eb
Darsh <msdarsh114@gmail.com>
2022-03-16 14:56:18 +0530

    first draft of homepage

---
+++ main.py
@@ -0,0 +1,8 @@
+n=int(input("Enter a Number : "))
+fact=1;
+if n==0:
+    print("the factorial of 0 is 1")
+else:
+    for i in range(1,n+1):
+        fact*=i
+    print("the factorial of ",n," is ",fact)
